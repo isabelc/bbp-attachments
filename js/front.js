496d0980fddfa2b704f32ff066b3ed7a6fbba7dc
Isabel <isabel@isabelcastillo.com>
2020-06-12 19:19:30 -0400

    spaces to tabs

--- js/front.js
+++ js/front.js
@@ -1,47 +1,47 @@
 /*jslint regexp: true, confusion: true, undef: true, sloppy: true, eqeq: true, vars: true, white: true, plusplus: true, maxerr: 50, indent: 4 */
 
 ;(function($, window, document, undefined) {
-    window.wp = window.wp || {};
-    window.wp.gdatt = window.wp.gdatt || {};
+	window.wp = window.wp || {};
+	window.wp.gdatt = window.wp.gdatt || {};
 
-    window.wp.gdatt.attachments = {
-        init: function() {
-          
-            $('#bbp-attachment-toggle').click(function () {
-                $('#bbp-attachment-upload').slideToggle();
-                $(this).toggleClass('bbp-attachment-toggle-active');
-                $('#bbp-attachment-toggle-wrap, .fep-form-field-fep_upload').toggleClass('palebg');
-            });            
+	window.wp.gdatt.attachments = {
+		init: function() {
+		  
+			$('#bbp-attachment-toggle').click(function () {
+				$('#bbp-attachment-upload').slideToggle();
+				$(this).toggleClass('bbp-attachment-toggle-active');
+				$('#bbp-attachment-toggle-wrap, .fep-form-field-fep_upload').toggleClass('palebg');
+			});            
 			$( document ).on( 'click', '.d4p-attachment-remove', function( e ) {
 				e.preventDefault();
 				$(this).closest('.bbp-attachment-field-input').replaceWith($(".bbp-attachment-field-input").val('').clone(true));
 				
 			});
 
-            $("form#new-post").attr("enctype", "multipart/form-data");
+			$("form#new-post").attr("enctype", "multipart/form-data");
 
-            $(document).on("click", ".d4p-bba-actions a", function(e){
-                return confirm(gdbbPressAttachmentsInit.are_you_sure);
-            });
+			$(document).on("click", ".d4p-bba-actions a", function(e){
+				return confirm(gdbbPressAttachmentsInit.are_you_sure);
+			});
 
-            $(document).on("click", ".d4p-attachment-addfile", function(e){
-                e.preventDefault();
+			$(document).on("click", ".d4p-attachment-addfile", function(e){
+				e.preventDefault();
 
-                var now = $(".bbp-attachments-form input[type=file]").length,
-                    max = parseInt(gdbbPressAttachmentsInit.max_files);
+				var now = $(".bbp-attachments-form input[type=file]").length,
+					max = parseInt(gdbbPressAttachmentsInit.max_files);
 
-                if (now < max) {
-                    $(this).before('<br><input type="file" size="40" name="d4p_attachment[]" class="bbp-attachment-field-input"><a href="#" class="d4p-attachment-remove">Remove</a><br>');
-                }
+				if (now < max) {
+					$(this).before('<br><input type="file" size="40" name="d4p_attachment[]" class="bbp-attachment-field-input"><a href="#" class="d4p-attachment-remove">Remove</a><br>');
+				}
 
-                if (now + 1 >= max) {
-                    $(this).remove();
-                }
-            });
-        }
-    };
+				if (now + 1 >= max) {
+					$(this).remove();
+				}
+			});
+		}
+	};
 
-    $(document).ready(function() {
-        wp.gdatt.attachments.init();
-    });
+	$(document).ready(function() {
+		wp.gdatt.attachments.init();
+	});
 })(jQuery, window, document);
